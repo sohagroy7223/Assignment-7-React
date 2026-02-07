c5d854632575c6870384f5a403991503f4c70119
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-08 00:43:22 +0600

    remove another away to solve

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Hero from "./components/Hero Section/Hero";
 import Main from "./components/Main Section/Main";
 import Navbar from "./components/Navbar/Navbar";
 
-const products = fetch("products.json").then((res) => res.json());
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
-      <Main products={products}></Main>
+      <Main></Main>
     </>
   );
 }
