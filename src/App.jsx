b735805ef17d56a5e17b9cb92e4c24f104f31f16
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-08 10:21:43 +0600

    addad tabile inthis file

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Main></Main>
+      <div className="overflow-x-auto rounded-box border border-base-content/5  flex text-black gap-3">
+        <table className="table w-[70%] bg-white ">
+          {/* head */}
+          <thead className="text-black">
+            <tr className="flex justify-between">
+              <th>Items</th>
+              <div className="flex  gap-20">
+                <th>Current Bid</th>
+                <th>Time Left</th>
+                <th>Bid Now</th>
+              </div>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+        <div className="bg-white w-[30%] rounded-xl h-80">
+          <h3>s</h3>
+        </div>
+      </div>
     </>
   );
 }
