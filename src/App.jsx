7848a072b296db1280d71991735525996484cc14
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-08 12:43:24 +0600

    addad tabil card iin this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { CiHeart } from "react-icons/ci";
 import Hero from "./components/Hero Section/Hero";
 import Main from "./components/Main Section/Main";
 import Navbar from "./components/Navbar/Navbar";
@@ -9,23 +10,55 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Main></Main>
-      <div className="overflow-x-auto rounded-box border border-base-content/5  flex text-black gap-3">
-        <table className="table w-[70%] bg-white ">
-          {/* head */}
-          <thead className="text-black">
-            <tr className="flex justify-between">
-              <th>Items</th>
-              <div className="flex  gap-20">
-                <th>Current Bid</th>
-                <th>Time Left</th>
-                <th>Bid Now</th>
-              </div>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
-        <div className="bg-white w-[30%] rounded-xl h-80">
-          <h3>s</h3>
+      <div className="main-container flex justify-between w-11/12 mx-auto text-center gap-3">
+        <div className="left-container w-[70%] bg-white rounded-2xl">
+          <div className="overflow-x-auto rounded-box border  text-black">
+            <table className="table">
+              {/* head */}
+              <thead className="text-black">
+                <tr>
+                  <th>items</th>
+                  <th>Current Bid</th>
+                  <th>Time Left</th>
+                  <th>Bid Now</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>1</th>
+                  <td>Cy Ganderton</td>
+                  <td>Quality Control Specialist</td>
+                  <td>Blue</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="right-container w-[30%] rounded-2xl bg-white">
+          <div className="w-full mx-auto p-5 items-center">
+            <div className="">
+              <h3 className="text-black">
+                {" "}
+                <CiHeart className="w-full mx-auto  items-center" size={25} />
+                <h3 className="text-xl font-medium">favorite items</h3>
+                <hr />
+                <div>
+                  <h3 className="mt-4 text-lg font-medium">
+                    Not Favorites yet
+                  </h3>
+                  <p className="mt-3 text-gray-600">
+                    Click the heart icon on any item to add it to your favorites
+                  </p>
+                  <hr />
+                  <div className="mt-3 flex justify-between">
+                    <h3 className="font-medium">Total bids Amount</h3>
+                    <p className="font-medium">000</p>
+                  </div>
+                </div>
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Main Section/Main.jsx
+++ src/components/Main Section/Main.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Main = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [Main]);
+  // console.log(products);
   return (
     <div className="w-11/12 mx-auto">
-      <h3 className="text-2xl font-bold mt-20">Active Auctions</h3>
+      <h3 className="text-2xl font-bold mt-20">
+        Active Auctions {products.length}
+      </h3>
       <p className="text-gray-300 mt-3">
         Discover and bid on extraordinary items
       </p>
