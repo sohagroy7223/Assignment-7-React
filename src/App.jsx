71a7aa2ba1e3442682e0e950dac55ad4805bd4f6
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-09 10:31:49 +0600

    addad tabile in this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Hero from "./components/Hero Section/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Products from "./components/Navbar/products/Products";
+
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+
+      <div className="main-container flex justify-between w-11/12 mx-auto gap-4">
+        <div className="left-container  bg-white w-[70%]">
+          <Products></Products>
+        </div>
+        <div className="right-container bg-white w-[30%]">r</div>
+      </div>
     </>
   );
 }
---
+++ src/components/Navbar/products/Products.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 text-black">
+        <table className="table ">
+          {/* head */}
+          <thead className="text-black">
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
